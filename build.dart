bf02f4efdb57d2cfdde62ba7ee31e14fb2d77766
Vadim Tsushko <vadimtsushko@gmail.com>
2016-07-04 14:25:54 +0500

    Dimensions

--- build.dart
+++ build.dart
@@ -4,7 +4,7 @@ import 'package:dogma_json_schema/build.dart';
 Future<Null> main(List<String> args) async {
   await build(
       args,
-      'sense_model', // The name of the package within the pubspec
+      'qe_model', // The name of the package within the pubspec
       'lib/models.json' // The path to the root JSON schema,
       );
 }
